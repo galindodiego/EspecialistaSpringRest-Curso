f9354591093c2e5ba721176dfae7a9304add6b44
Diego Galindo <diegogalindo84@gmail.com>
2023-02-13 15:42:47 -0300

    Implementado recursos da Entidade Cidade

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,7 @@ public class CidadeRepositoryImpl implements CidadeRepository {
 		return manager.find(Cidade.class, id);
 	}
 
+	@Transactional
 	@Override
 	public Cidade salvar(Cidade cidade) {
 		return manager.merge(cidade);
@@ -35,8 +37,11 @@ public class CidadeRepositoryImpl implements CidadeRepository {
 
 	@Override
 	@Transactional
-	public void remover(Cidade cidade) {
-		cidade = buscar(cidade.getId());
+	public void remover(Long id) {
+		Cidade cidade = buscar(id);
+		if(cidade == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
 		manager.remove(cidade);
 		
 	}
