781807f597a3d9d372378381cbeeebb79d87023d
Diego Galindo <diegogalindo84@gmail.com>
2023-03-03 20:11:03 -0400

    Adicionado pacote Specifications para implementações futuras

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.domain.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,6 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 
 	
-//	List<Cozinha> consultarPorNome(String nome); 
+	List<Cozinha> nome(String nome); 
 
 }
