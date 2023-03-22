437f61159b31ff443098bfb9541467002b4a5c0f
Diego Galindo <diegogalindo84@gmail.com>
2023-03-22 18:52:29 -0400

    Desafio - tratando a exceção NoHandlerFoundException

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,17 +5,17 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-	PARAMETRO_INVALIDO("/parametro-invalido","Parametro inválido"),
-	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada","Entidade não encontrada"),
-	ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio","Violação de regra de negócio"),
-	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensível");
-	
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parametro inválido"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível");
+
 	private String title;
 	private String uri;
-	
-	ProblemType(String path, String title){
-		this.uri = "https://algafood.com.br"+ path;
+
+	ProblemType(String path, String title) {
+		this.uri = "https://algafood.com.br" + path;
 		this.title = title;
 	}
 }
