1737b5d9f692f2b543f40428834206e3b4832688
Diego Galindo <diegogalindo84@gmail.com>
2023-03-27 14:57:43 -0400

    Adicionando constraints e validando no controller com @Valid

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
@@ -53,7 +54,7 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Restaurante adicionar (@RequestBody Restaurante restaurante) {
+	public Restaurante adicionar (@RequestBody @Valid Restaurante restaurante) {
 		
 		try {
 		
