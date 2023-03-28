72d5e8b63b148ba11375b3fda68f4b16684e3d08
Diego Galindo <diegogalindo84@gmail.com>
2023-03-28 16:44:56 -0400

    ESR - 09.11 - Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -55,7 +55,7 @@ public class CidadeController {
 	}
 
 	@PutMapping("/{cidadeId}")
-	public Cidade atualizar(@PathVariable Long cidadeId, @RequestBody @Valid Cidade cidade) {
+	public Cidade atualizar(@PathVariable Long cidadeId, @RequestBody @Valid  Cidade cidade) {
 		
 		try {
 			Cidade cidadeAtual = cadastroCidadeService.buscarOuFalhar(cidadeId);
