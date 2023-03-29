728d82fcef0df99ee6b2440d81c386995d0aa1c9
Diego Galindo <diegogalindo84@gmail.com>
2023-03-29 14:00:53 -0400

    ESR - 09.12 - Desafio - customizando mensagens de validação

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -49,6 +49,7 @@ public class Restaurante {
 	private String nome;
 
 	//@DecimalMin("1")
+	@NotNull
 	@PositiveOrZero 
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
