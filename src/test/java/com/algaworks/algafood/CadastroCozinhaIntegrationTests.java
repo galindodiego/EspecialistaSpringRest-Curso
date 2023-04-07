2c3191f43e18b65bc542dd60726ebe6a10891f71
Diego Galindo <diegogalindo84@gmail.com>
2023-04-07 13:51:12 -0400

    ESR - 10.05 - Desafio - escrevendo testes de integração

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -8,8 +8,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
@@ -22,7 +27,7 @@ public class CadastroCozinhaIntegrationTests {
 	
 	
 	@Test
-	public void testarCadastroCozinhaComSucesso() {
+	public void CadastroCozinhaComSucesso() {
 		// cenário
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
@@ -31,19 +36,29 @@ public class CadastroCozinhaIntegrationTests {
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
 		
 		//validação
-		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha).isNotNull(); 
 		assertThat(novaCozinha.getId()).isNotNull();
 		
 	}
 	
 	@Test(expected = ConstraintViolationException.class)
-	public void testarCadastrarCozinhaSemNome() {
+	public void CadastrarCozinhaSemNome() {
 		
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
 		
 		
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+	}
+	
+	@Test(expected = EntidadeEmUsoException.class)
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		cadastroCozinha.excluir(1l);
+	}
+	
+	@Test(expected = CozinhaNaoEncontradaException.class)
+	public void DeveFalhar_QuandoExcluirCozinhaInexistente() {
+		cadastroCozinha.excluir(99l);
 	}
 
 }
